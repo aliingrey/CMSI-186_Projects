419214c04604edbb3797ee8d84925028cafd6214
aliingrey <35271481+aliingrey@users.noreply.github.com>
2018-01-15 22:37:43 -0800

    Update Hello.java

--- homework00/Hello.java
+++ homework00/Hello.java
@@ -1,3 +1,20 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.java
+ *  Purpose       :  Says "Hello world"
+ *  Author        :  Ali Ingrey
+ *  Date          :  2018-01-15
+ *  Description   :  prints "Hello, world!"
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-02  Ali Ingrey Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 public class HelloWorld {
 	public static void main ( String [] args ) {
 		System.out.println( “Hello, world!” );
