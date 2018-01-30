fe44f40c6a7bca500c0c1e151c68fb557124d20a
aliingrey <35271481+aliingrey@users.noreply.github.com>
2018-01-30 09:50:47 -0800

    containsVowel and isPalindrome

---
+++ homework02/StringStuff.java
@@ -0,0 +1,40 @@
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class StringStuff {
+
+
+  /**
+   * Method to determine if a string contains one of the vowels: A, E, I, O, U, and sometimes Y.
+   * Both lower and upper case letters are handled.  In this case, the normal English rule for Y means
+   * it gets included.
+   *
+   * @param s String containing the data to be checked for &quot;vowel-ness&quot;
+   * @return  boolean which is true if there is a vowel, or false otherwise
+   */
+   public static boolean containsVowel( String s ) {
+       for (int i = 0; i < 5; i++) {
+        if ((s.charAt(i) == 'a' ) || (s.charAt(i) == 'e')  || (s.charAt(i) == 'i') || (s.charAt(i) == 'o') || (s.charAt(i) == 'u')) {
+               return true;
+        } else {
+            if (s.charAt(i) == 'y') {
+                return true;
+            } else {
+                return false;
+            }
+    
+    }
+
+
+  public static boolean isPalindrome( String s) {
+      String reverse = ""; //empty string to add to 
+      for (int i = s.length - 1; i >= 0; i--) {
+          reverse = reverse + s.charAt(i);
+      }
+      if (s == reverse) {
+          return true;
+        } else {
+          return false;
+        }
+      return true;
+   }
