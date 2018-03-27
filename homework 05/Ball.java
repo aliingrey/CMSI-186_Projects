e1b0d2d2010b54c16cb3803a244e58bc689a6eef
aliingrey <35271481+aliingrey@users.noreply.github.com>
2018-03-26 23:00:15 -0700

    Update Ball.java

--- homework 05/Ball.java
+++ homework 05/Ball.java
@@ -1,4 +1,4 @@
-//import java.text.DecimalFormat;
+import java.text.DecimalFormat;
 
 public class Ball {
 
@@ -12,103 +12,135 @@ public class Ball {
 	private double xSpeed;
 	private double ySpeed;
 	private String ballString;
+	private double timeSlice;
 
 	private static final double FRICTION = 0.01;
-	//at time 0, they're all put into motion at random speeds
 
-	//constructor
-	public Ball(double xPosition, double yPosition, double xSpeed, double ySpeed) {
+	public Ball(double xPosition, double yPosition, double xSpeed, double ySpeed, double userTimeSlice) {
 		this.xPosition = xPosition; //m/s across
 		this.yPosition = yPosition; //m/s up/down
 		this.xSpeed = xSpeed;
 		this.ySpeed = ySpeed;
+		this.timeSlice = userTimeSlice;
 	}
 
+	public void moveBall() {
+		xPosition += xSpeed; //*timeSlice
+		xSpeed -= xSpeed * FRICTION;
+
+		yPosition = yPosition;
+		ySpeed -= ySpeed * FRICTION;
+	}
+	
 	public double getVelocity() {
-		ballVelocity = (Math.pow(xSpeed,2)) + (Math.pow(ySpeed,2));
-		ballVelocity = Math.sqrt(ballVelocity);
+		ballVelocity = Math.sqrt( (xSpeed * xSpeed) + (ySpeed * ySpeed) );
 		//System.out.println("Initial velocity: " + ballVelocity);
 		return ballVelocity;
-		//pthagrem theorem/totalSeconds
 	}
 
-	/*
-	Friction acts to slow each ball down until it comes to rest. 
-	Simulate friction that continuously decreases each ball's speed 
-	@ the rate of one per cent per second until it is traveling less than one inch per second
-	at which point it comes to rest.
-	*/
-
-	public double xFriction() { //what happens when velocity are neg numbers
+	public double UpdateXSpeed() { //what happens when velocity are neg numbers
+		/*
 		xSpeed -= xSpeed * FRICTION;
+		System.out.println("xSpeed: " + xSpeed);
+		*/
 		return xSpeed;
 	}
-	public double yFriction() { //what happens when velocity are neg numbers
+	public double UpdateYSpeed() { //what happens when velocity are neg numbers
+		/*
 		ySpeed -= ySpeed * FRICTION;
+		System.out.println("ySpeed: " + ySpeed);
+		*/
 		return ySpeed;
 	}
 	//could do a field class (Field.java)
 
-	/*
-	data about each ball will be given to your program via four consecutive args
-	namely the x- and y-coordinates of the ball's starting position [measured in feet]
-	followed by its speeds in the x- and y-directions [in feet per second].
-	*/
 	public double UpdateXPosition() {
+		/*
 		xPosition += xSpeed;
-   		//System.out.println("new x position: " + xPosition);
+		System.out.println("x pos: " + xPosition);
+		*/
    		return xPosition;
-
 	}
 
 	public double UpdateYPosition() {
+		/*
 		yPosition += ySpeed;
-		//System.out.println("new y position: " + yPosition);
+		System.out.println("y pos: " + yPosition);
+		*/
 		return yPosition;
 	}
 
-		public String toString() {
-		//current x and y position
-		//current x and y velocity values
-		//total movement
-		if (xSpeed == 0 && ySpeed == 0) {
-			//traveling less than one inch per second, at which point it comes to rest.
-			ballString = "POSITOIN " + UpdateXPosition() + ", " + UpdateYPosition() + "    <at rest>";
+	public boolean atRest() {
+		if ( Math.sqrt( (xSpeed * xSpeed) + (ySpeed * ySpeed) ) < 0.083 ) {
+			return true;
 		} else {
-			ballString = "POSITOIN " + UpdateXPosition() + ", " + UpdateYPosition() + "    SPEED " + xFriction() + ", " + yFriction() + " m/s";
+     		return false;
 		}
+	}
 
-		//velocity, if 0 - report @ rest
+	public String toString() {
+		String stringFormat = "00.0000";
+		DecimalFormat outputFormat = new DecimalFormat( stringFormat );
+
+		if (atRest() == true) {
+			ballString = "POSITOIN " + outputFormat.format(xPosition) + ", " + outputFormat.format(yPosition) + "    <at rest>";
+		} else {
+			ballString = "POSITOIN " + outputFormat.format(xPosition) + ", " + outputFormat.format(yPosition) + "    SPEED " + outputFormat.format(xSpeed) + ", " + outputFormat.format(ySpeed) + " m/s";
+		}
 		System.out.println(ballString);
 		return ballString;
 	}
 	
 	public static void main( String args[] ) {
+    System.out.println( "\nBaLL CLASS TESTER PROGRAM\n" +
+                          "--------------------------\n" );
 
 		System.out.println("\nnew ball - b1");
-		Ball b1 = new Ball(10, 50, 2, 6);
+		Ball b1 = new Ball(10, 50, 2, 6, 30);
+		System.out.println( "The velocity of the ball is " + b1.getVelocity() + " ft/s.");
+		b1.moveBall();
 		b1.toString();
+		b1.moveBall();
 		b1.toString();
+		b1.moveBall();
+		b1.toString();
+		b1.moveBall();
+		b1.toString();
+		b1.moveBall();
 		b1.toString();
 		System.out.println("\nnew ball - b2");
-		Ball b2 = new Ball(20, 60, 3, 7);
+		Ball b2 = new Ball(20, 60, 3, 7, 30);
+		b2.moveBall();
 		b2.toString();
+		b2.moveBall();
 		b2.toString();
+		b2.moveBall();
 		b2.toString();
+		b2.moveBall();
 		b2.toString();
 		System.out.println("\nnew ball - b3");
-		Ball b3 = new Ball(30, 70, 4, 8);
+		Ball b3 = new Ball(30, 70, 4, 8, 30);
+		b3.moveBall();
 		b3.toString();
+		b3.moveBall();
 		b3.toString();
+		b3.moveBall();
 		b3.toString();
+		b3.moveBall();
 		b3.toString();
 		System.out.println("\nnew ball - b4");
-		Ball b4 = new Ball(40, 80, 5, 9);
+		Ball b4 = new Ball(40, 80, 5, 9, 30);
+		b4.moveBall();
 		b4.toString();
+		b4.moveBall();
 		b4.toString();
+		b4.moveBall();
 		b4.toString();
+		b4.moveBall();
 		b4.toString();
 		//System.out.println("Your ball starts with a velocity of: " + (int)v0  + "meters per second");
+
+
 		
 	}
 
