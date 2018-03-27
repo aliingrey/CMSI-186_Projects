a13cb7032e5621798109e74d648619676ec70eb2
aliingrey <35271481+aliingrey@users.noreply.github.com>
2018-03-27 00:31:01 -0700

    Update SoccerSim.java

--- homework 05/SoccerSim.java
+++ homework 05/SoccerSim.java
@@ -1,118 +1,87 @@
-//do any of the balls bump into each other and or hit the pole
-//simulated time of the first collision --> output collision
-public class SoccerSim{
-	//private Ball ball = null;
-	private static Ball[] ballArray = null;
-	private static int ballCount = 0;
-	private Timer stopWatch = null;
-	private static int j = 0;
-
-	private static final double POLE = 15;
-	//field
-	private static final double CENTERFIELD_X = 0;
-	private static final double CENTERFIELD_Y = 0;
-	private static final double OUTOFBOUNDS_X = 45;
-	private static final double OUTOFBOUNDS_Y = 60;
+public class SoccerSim {
+  private final double RADIUS = 4.45;
+  private final double WEIGHT = 1;
 	
-	double DEFAULT_TIMESLICE = 1;
+  private static final double DEFAULT_TIME_SLICE = 30;
 
-	public SoccerSim(){
+  private static final double FIELD_WIDTH = 1000.0;
+  private static final double FIELD_HEIGHT = 1000.0;
 
-		//
-		/*
- 		for (int i = 0; i < c; i ++) {
-        	ds[i] = new Die(s);
-      	
-      	in main:
-      	DiceSet ds = new DiceSet(4, 6);
-      	
+  private static final double X_POLE = 10;
+  private static final double Y_POLE = 10;
+
+  private double timeSlice = 1.0;
+  private int ballCount = 0;
+  
+  private Ball[] ballArray = null;
+  private Clock soccerClock = null;
+
+  public SoccerSim() {
+    soccerClock = new Clock();
+  }
+	
+  public boolean findCollision() {
+    for (int i = 0; i < ballArray.length - 1; i++) {
+      if ( ( Math.abs(ballArray[i].UpdateXPosition()) - X_POLE <= RADIUS ) && ( Math.abs(ballArray[i].UpdateYPosition()) - Y_POLE <= RADIUS ) ) {
+        System.out.println("the ball hit the pole!");
+        return true;
       }
-	*/
-	}
-	public boolean checkCollision(double x2, double y2){
-    if ((Math.abs(x-x2) <= RADIUS) && (Math.abs(y-y2) <= RADIUS)) {
-      return true;
-    } else {
-      return false; 
-    }
-    /*
-    public void positonBall(double timeSlice){
-    	x = x + speedX*timeSlice;
-    	y = y + speedY*timeSlice;
+      for (int j = i + 1; j < ballArray.length; j++) {
+        if ( (Math.abs(ballArray[i].UpdateXPosition() - ballArray[j].UpdateXPosition()) <= RADIUS) && (Math.abs(ballArray[i].UpdateYPosition() - ballArray[j].UpdateYPosition() <= RADIUS))) {
+          System.out.println("the ball hit another ball!");
+          return true;
+        }
+       }
+     }
    }
-*/
-	/*
-	if (Ball.length > 1) {
-
-	}
-
-   public boolean checkCollision(double x2, double y2){
-    if ((Math.abs(x-x2) <= RADIUS) && (Math.abs(y-y2) <= RADIUS)) {
-      return true;
-    } else {
-      return false; 
+   public boolean outOfBounds() {
+    for (int i = 0; i < ballArray.length - 1; i++) {
+      if ( (ballArray[i] > FIELD_HEIGHT) || (ballArray[i] < -FIELD_HEIGHT) ) {
+        System.out.println("ob from the y direction");
+        return true;
+      } else if ( (ballArray[i] > FIELD_WIDTH) || (ballArray[i] < -FIELD_WIDTH) ) {
+        System.out.println("ob from the x direction");
+        return true;
+      } else {
+        return false;
+      }
     }
    }
-   */
-	//args: x1, y1, xv1, x2, y2, xv2 (new ball) x1, y1 ...
-	
-	//creating multiple balls
-	//collision cheeck
-	public static void main( String args[] ) {
-	  //SoccerSim ballArray = new SoccerSim();
-	  System.out.println( "\n   Starting the SoccerSim game!\n\n" );
-	  
-	  Timer a = new Timer(60);
-	  Ball b1 = new Ball(0, 0, 0, 0);
-	  Ball pole = new Ball(0, 0, 0, 0);
-	  
+  public static void main(String[] args[]) {
 
-	  if( 0 == args.length ) {
+    if( 0 == args.length ) {
          System.out.println( "   Sorry you must enter at least one argument\n" +
                              "   Please try again..........." );
          System.exit( 1 );
-      } 
-      
-	  if ((args.length % 4) == 0) {
-	  	ballCount = args.length/4;
-	  	timer = new Timer( DEFAULT_TIMESLICE );
-	  	System.out.println("you've created " + args.length/4 + " balls");
-	  }
+    } 
+    if ((args.length % 4) == 1) {
+      double timeSlice = Double.parseDouble(args[4]);
+      if ( (args.length - 1) % 4 == 0) {
+        ballCount = args.length/4;
+        System.out.println("you've created " + ballCount + " balls");
+      }
+    }
+    
+    if ((args.length % 4) == 0) {
+      ballCount = args.length/4;
+      timer = new Timer( DEFAULT_TIMESLICE );
+      System.out.println("you've created " + args.length/4 + " balls");
+    }
 
-	  if ((args.length % 4) == 1) {
-	  	double timeSlice = Double.parseDouble(args[4]);
-	  	if ( (args.length - 1) % 4 == 0) {
-			ballCount = args.length/4;
-	  	}
-	  }
+   
 
-	  pole = new Ball( (double)Math.floor( 10 , 10 , 0 , 0);
+    ballArray = new Ball[ballCount];
 
-	  //ballArray = new Ball[ballCount];
-	  
+    int j = 0;
+    
+    for (int i = 0; i < ballArray.length; i++)  {
+        ballArray[i] = new Ball(Double.parseDouble(args[(j + 0)]), Double.parseDouble(args[(j + 1)]), Double.parseDouble(args[(j + 2)]), Double.parseDouble(args[(j + 3)]));
+        j += 4;
+    }
 
 
-	/*
-	  for (int j = 0; j < args.length; j+=4) {
-	  	balls[j] = new Ball(xPosition, yPosition, xSpeed, ySpeed);
-	  }
-	  */
-
-	  for (int i = 0; i < args.length; i+=4) { //loop through all the arguments
-		double xPosition = Double.parseDouble(args[i + 0]);
-		System.out.println("x posiiton: " + xPosition);
-	  	double yPosition = Double.parseDouble(args[i + 1]);
-	  	double xSpeed = Double.parseDouble(args[i + 2]);
-	  	double ySpeed = Double.parseDouble(args[i + 3]);
-		//Ball ball = new Ball(xPosition, yPosition, xSpeed, ySpeed);
-
-		j++; 
-		ballArray[j] = new Ball(xPosition, yPosition, xSpeed, ySpeed);
-		//balls[j] = new Ball(Double.parseDouble(args[i + 0]), Double.parseDouble(args[i + 1]), Double.parseDouble(args[i + 2]), Double.parseDouble(args[i + 3]));//j keeps count of how many balls there are
-		//Ball ball = new Ball [j];
-		//System.out.println(j);
-	  }
-	  System.out.println("balll array: " + ballArray);
-	  
-	}
-}
+    System.out.println("\n  Hello, world, from the SoccerSim program!");
+    SoccerSim localSoccerSim = new SoccerSim();
+     }
+  }
+	
